71736bf8999c15479f532aeb7faa7590a67bca9d
Leonel <leonelbasilio911@gmail.com>
2022-05-09 16:18:49 -0300

    Projeto criado

---
+++ src/test/java/com/springboot/cursoudemy/CursoUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.cursoudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoUdemyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
