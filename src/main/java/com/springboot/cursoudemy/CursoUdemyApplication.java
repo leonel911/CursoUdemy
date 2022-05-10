c73c167536f281cdcbcc87fd538fff1f245d7a8c
Leonel <leonelbasilio911@gmail.com>
2022-05-09 21:30:36 -0300

    Criando operacao de instanciacao

--- src/main/java/com/springboot/cursoudemy/CursoUdemyApplication.java
+++ src/main/java/com/springboot/cursoudemy/CursoUdemyApplication.java
@@ -1,13 +1,31 @@
 package com.springboot.cursoudemy;
 
+import com.springboot.cursoudemy.domain.Categoria;
+import com.springboot.cursoudemy.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursoUdemyApplication {
+public class CursoUdemyApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CursoUdemyApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2)); 
+
+    }
 }
--- src/main/java/com/springboot/cursoudemy/repositories/CategoriaRepository.java
+++ src/main/java/com/springboot/cursoudemy/repositories/CategoriaRepository.java
@@ -9,4 +9,6 @@ public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
 
 
 
+
+
 }
