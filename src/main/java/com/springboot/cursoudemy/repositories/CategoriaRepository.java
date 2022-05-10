f1a915b073999228e044f02f28901324a4d80402
Leonel <leonelbasilio911@gmail.com>
2022-05-09 21:09:48 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/springboot/cursoudemy/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.springboot.cursoudemy.repositories;
+
+import com.springboot.cursoudemy.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+
+
+}
---
+++ src/main/java/com/springboot/cursoudemy/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.springboot.cursoudemy.services;
+
+import com.springboot.cursoudemy.domain.Categoria;
+import com.springboot.cursoudemy.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public Categoria find(Integer id) {
+        Optional<Categoria> obj = categoriaRepository.findById(id);
+        return obj.orElse(null);
+
+    }
+
+}
