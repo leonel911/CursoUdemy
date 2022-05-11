9d17b2fcc4648f80f08776c98e864043c462a42e
Leonel <leonelbasilio911@gmail.com>
2022-05-11 20:43:01 -0300

    Ajustes no endpoint /categorias/{id};

--- src/main/java/com/springboot/cursoudemy/services/CategoriaService.java
+++ src/main/java/com/springboot/cursoudemy/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.springboot.cursoudemy.services;
 
 import com.springboot.cursoudemy.domain.Categoria;
 import com.springboot.cursoudemy.repositories.CategoriaRepository;
+import com.springboot.cursoudemy.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,8 +16,8 @@ public class CategoriaService {
 
     public Categoria find(Integer id) {
         Optional<Categoria> obj = categoriaRepository.findById(id);
-        return obj.orElse(null);
-
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 
 }
