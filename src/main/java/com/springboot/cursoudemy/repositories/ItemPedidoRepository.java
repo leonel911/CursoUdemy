39476c671bcfd26b6d8c949e1ebf3a26cb80dce5
Leonel <leonelbasilio911@gmail.com>
2022-05-12 23:19:52 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/springboot/cursoudemy/repositories/ItemPedidoRepository.java
@@ -0,0 +1,14 @@
+package com.springboot.cursoudemy.repositories;
+
+import com.springboot.cursoudemy.domain.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+
+
+
+
+}
